c82412a68aab6c8988d8b2381d972ce679d0877a
Varduhi <vaduhipetikyan@gmail.com>
2026-08-11 22:47:49 +0400

    verjnakany

--- HomeworkVard.py
+++ HomeworkVard.py
@@ -1,7 +1,7 @@
 class Product:
-    def __init__(self, name, prise, stock=10):
+    def __init__(self, name, price, stock=10):
         self.name=name
-        self.prise=prise
+        self.price=price
         self.stock=stock
 class ShoppingCard:
        def __init__(self):
@@ -16,14 +16,31 @@ class ShoppingCard:
        def get_total(self):
             total=0
             for i in self.items:
-                total+=i["product"].prise * i["quantity"]
+                total+=i["product"].price * i["quantity"]
             return total
+       def remove_product(self, product, count):
+        
+        for i in self.items:
+            
+            if i["product"] == product:
+                
+                product.stock += i["quantity"]
+                
+               
+                self.items.remove(i)
+                
+                print(f"{product.name}-ը հեռացվեց զամբյուղից:")
+                return  
+        
+        
+        print(f"{product.name}-ը չկա ձեր զամբյուղում:")
         
 pr1 = Product("Phone", 500, stock=5)
 pr2 = Product("Headphones", 50, stock=10)
 
 cart = ShoppingCard()
-cart.add_product(pr1, 2)  # 2 * 500 = 1000
-cart.add_product(pr2, 3)  # 3 * 50 = 150
+cart.add_product(pr1, 2) 
+cart.add_product(pr2, 3)  
+cart.remove_product(pr1, 3)
 
-print("Total:", cart.get_total())  # Կտպի 1150
+print("Total:", cart.get_total())  
